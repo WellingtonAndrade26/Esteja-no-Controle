157a009ed62ea581418d304a5377f0d7ea3a25ed
Wellington Andrade <web3explorer01@gmail.com>
2026-08-31 11:41:40 -0300

    Atualizar app para 1.0.10

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window.ENC_CONFIG = {
   supabaseUrl: "https://iyfknonepcarerlpcxlu.supabase.co",
   supabasePublishableKey: "sb_publishable_V4zEvBC8ZNYN8vSzmBq_rg_2EPjFqYZ",
   appName: "Esteja no Controle",
-  appVersion: "1.0.9",
+  appVersion: "1.0.10",
   publicBaseUrl: "",
   legalOwner: "Wellington Porto de Andrade",
   supportEmail: "",
@@ -12,7 +12,7 @@ window.ENC_CONFIG = {
 (() => {
   if (document.querySelector('script[data-enc-card-management]')) return;
   const script = document.createElement("script");
-  script.src = "./js/card-management.js?v=1.0.9";
+  script.src = "./js/card-management.js?v=1.0.10";
   script.async = true;
   script.dataset.encCardManagement = "1";
   document.head.appendChild(script);
