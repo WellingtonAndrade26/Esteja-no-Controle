53e65fdb264595ceb46ca42b44d8583b4a01e278
Wellington Andrade <web3explorer01@gmail.com>
2026-08-31 10:25:20 -0300

    Atualizar versão para 1.0.7

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window.ENC_CONFIG = {
   supabaseUrl: "https://iyfknonepcarerlpcxlu.supabase.co",
   supabasePublishableKey: "sb_publishable_V4zEvBC8ZNYN8vSzmBq_rg_2EPjFqYZ",
   appName: "Esteja no Controle",
-  appVersion: "1.0.6",
+  appVersion: "1.0.7",
   publicBaseUrl: "",
   legalOwner: "Wellington Porto de Andrade",
   supportEmail: "",
@@ -12,7 +12,7 @@ window.ENC_CONFIG = {
 (() => {
   if (document.querySelector('script[data-enc-card-management]')) return;
   const script = document.createElement("script");
-  script.src = "./js/card-management.js?v=1.0.6";
+  script.src = "./js/card-management.js?v=1.0.7";
   script.async = true;
   script.dataset.encCardManagement = "1";
   document.head.appendChild(script);
