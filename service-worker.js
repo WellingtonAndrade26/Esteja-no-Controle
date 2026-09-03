c1547616b1be247e650892671c043d24703a58b2
Wellington Andrade <web3explorer01@gmail.com>
2026-09-03 14:09:29 -0300

    release: cache Vales navigation in 1.0.14

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "esteja-no-controle-1-0-13";
+const CACHE = "esteja-no-controle-1-0-14";
 const CORE = [
   "./",
   "./index.html",
@@ -17,6 +17,7 @@ const CORE = [
   "./js/card-limit-sync.js",
   "./js/ai-complete.js",
   "./js/benefits.js",
+  "./js/benefits-keeper.js",
   "./manifest.json",
   "./assets/icon-192.png",
   "./assets/icon-512.png",
